77e2bc96b7574eb0c5586e3ef8a98bea35dca731
fujioka <rodrigofujioka@gmail.com>
2026-05-09 07:57:26 -0300

    Ajustes para aula

--- src/main/java/br/com/alunoonline/api/exception/RestControllerException.java
+++ src/main/java/br/com/alunoonline/api/exception/RestControllerException.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.data.core.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
